af8a9e8ff0dfff9f6926d511fd3bccd81e8f1084
initial commit <>
2019-03-15 20:02:23 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <chart
+      v-for="(chart, index) in [ ...chartData[0] ]"
+      :key="index"
+      :chartData="chart"
+    />
+  </div>
+</template>
+
+<script>
+import ChartDataJSON from '@/assets/chart_data.json';
+import Chart from '@/components/Chart/Chart.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Chart,
+  },
+  data() {
+    return {
+      chartData: ChartDataJSON,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
