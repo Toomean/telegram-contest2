6ddde4313176ed77f02e6fcd722fd962039b8603
Artem Tumin <>
2019-03-21 01:53:07 +0300

    finish zoom components

--- src/components/Chart/ChartZoom/ChartZoom.vue
+++ src/components/Chart/ChartZoom/ChartZoom.vue
@@ -1,56 +1,110 @@
 <template>
-    <div class="chart-zoom">
-        <div class="chart-zoom__left">
-
-        </div>
-        <div class="chart-zoom__window">
-            <svg xmlns="http://www.w3.org/2000/svg"
-                width="100%"
-                height="100%"
-            >
-                <slot />
-            </svg>
-        </div>
-        <div class="chart-zoom__right">
-
-        </div>
+    <div class="chart-zoom"
+        ref="zoom"
+    >
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+        >
+            <slot />
+        </svg>
+        <vue-drag-resize
+            :x="left"
+            :isActive="true"
+            :w="width"
+            :h="96"
+            :axis="'x'"
+            :sticks="['ml','mr']"
+            v-on:resizing="resize"
+            v-on:dragging="resize"
+            :parentLimitation="true"
+        />
     </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      width: 200,
+      height: 0,
+      top: 0,
+      left: 0,
+    };
+  },
+  mounted() {
+    this.left = this.$refs.zoom.clientWidth - this.width;
+  },
+  methods: {
+    resize({
+      width, height, top, left,
+    }) {
+      this.width = width;
+      this.height = height;
+      this.top = top;
+      this.left = left;
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .chart-zoom {
     position: relative;
-    margin: 1em 0;
-    height: 5em;
-    background: #f5f9fb;
+    height: 6em;
+    margin: 1em 0 0;
 
-    &__left,
-    &__right {
-        position: absolute;
-        top: 0;
-        left: 0;
+    overflow-x: hidden;
 
-        width: .5em;
-        height: 100%;
-
-        background: #ddeaf3;
-
-        cursor: ew-resize;
+    .vdr {
+        &.active {
+            &:before {
+                outline: none;
+                border: 1px solid rgba(135, 171, 196, .4);
+                left: 8px;
+                right: 8px;
+            }
+        }
     }
 
-    &__right {
-        left: 100%;
-        transform: translateX(-100%);
-    }
+    .vdr-stick {
+        top: 0 !important;
 
-    &__window {
-        height: 100%;
+        height: 100% !important;
+        margin: 0 !important;
+        border: none;
+        background: rgba(135, 171, 196, .4);
+        box-shadow: none;
+
+        &.vdr-stick-ml,
+        &.vdr-stick-mr {
+
+            &::before {
+                content: '';
+                position: absolute;
+                top: 0;
+                width: 100vw;
+                height: 100%;
+
+                background: rgba(245, 249, 251, .8);
+            }
+        }
+
+        &.vdr-stick-ml {
+            left: 0 !important;
+
+            &::before {
+                right: 100%;
+            }
+        }
+
+        &.vdr-stick-mr {
+            right: 0 !important;
+
+            &::before {
+                left: 100%;
+            }
+        }
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+import VueDragResize from 'vue-drag-resize';
+
 import store from './store';
 import App from './App.vue';
 
 library.add(faCheck);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('vue-drag-resize', VueDragResize);
 
 Vue.config.productionTip = false;
 
